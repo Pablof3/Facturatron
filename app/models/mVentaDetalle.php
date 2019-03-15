77111256abb15918936f13e221338c70e04ba24f
Mauritius Gamarra <mauriciogamarravargas@gmail.com>
2019-03-15 11:40:36 -0400

    cambios factura

--- app/models/mVentaDetalle.php
+++ app/models/mVentaDetalle.php
@@ -8,29 +8,29 @@ class mVentaDetalle
         $this->db = new db;
     }
 
-    public function Insertar($VentaDetalle)
+    public function Insertar($ventadetalle)
     {
         $query="INSERT INTO VentaDetalle(venta, producto, cantidad, subtotal)
                 VALUES(:venta, :producto, :cantidad, :subtotal)";
                 $this->db->prepare($query);
-                $this->db->bindParam(':venta', $VentaDetalle->venta);
-                $this->db->bindParam(':producto', $VentaDetalle->producto);
-                $this->db->bindParam(':cantidad', $VentaDetalle->cantidad);
-                $this->db->bindParam(':subtotal', $VentaDetalle->subtotal);
+                $this->db->bindParam(':venta', $ventadetalle->venta);
+                $this->db->bindParam(':producto', $ventadetalle->producto);
+                $this->db->bindParam(':cantidad', $ventadetalle->cantidad);
+                $this->db->bindParam(':subtotal', $ventadetalle->subtotal);
                 return $this->db->execute();
     }
 
-    public function Actualizar($VentaDetalle)
+    public function Actualizar($ventadetalle)
     {
         $query="UPDATE VentaDetalle
                 SET venta= :venta, producto= :producto, cantidad= :cantidad, subtotal= :subtotal
                 WHERE id_ventadetalle= :id_ventadetalle";
                 $this->db->prepare($query);
-                $this->db->bindParam(':venta', $VentaDetalle->venta);
-                $this->db->bindParam(':producto', $VentaDetalle->producto);
-                $this->db->bindParam(':cantidad', $VentaDetalle->cantidad);
-                $this->db->bindParam(':subtotal', $VentaDetalle->subtotal);
-                $this->db->bindParam(':id_ventadetalle', $VentaDetalle->id_ventadetalle);
+                $this->db->bindParam(':venta', $ventadetalle->venta);
+                $this->db->bindParam(':producto', $ventadetalle->producto);
+                $this->db->bindParam(':cantidad', $ventadetalle->cantidad);
+                $this->db->bindParam(':subtotal', $ventadetalle->subtotal);
+                $this->db->bindParam(':id_ventadetalle', $ventadetalle->id_ventadetalle);
                 return $this->db->execute();
     }
 
