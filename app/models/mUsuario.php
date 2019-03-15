0da5b1c52682fea1e884a49631b13c9d679abb89
Pablof3 <PabloHy2898@hotmail.com>
2019-03-15 01:49:17 -0400

    mUsuario

--- app/models/mUsuario.php
+++ app/models/mUsuario.php
@@ -1,20 +1,41 @@
 <?php
 class mUsuario 
 {
-    private $db;
+    private $db=new Database;
     public function __construct() {
         $this->db=new Database;
     }
 
-    public function Insertar()
+    public function Insertar($usuario)
     {
-
+        $query="INSERT INTO Usuario(usuario, password, esAdmin, ci, nombre, apellido)
+                VALUES (:usuario, :password, :esAdmin, :ci, :nombre, :apellido)";
+        $this->db->prepare($query);
+        $this->db->bindParam(':usuario',$usuario->usuario);
+        $this->db->bindParam(':password',$usuario->password);
+        $this->db->bindParam(':esAdmin',$usuario->esAdmin);
+        $this->db->bindParam(':ci',$usuario->ci);
+        $this->db->bindParam(':nombre',$usuario->nombre);
+        $this->db->bindParam(':nombre',$usuario->apellido);
+        return $this->db->execute();
+        
     }
-    public function Actualizar()
+    public function Actualizar($usuario)
     {
-
-    }
-    public function Eliminar()
+        $query="UPDATE Usuario 
+                SET usuario=:usuario, password=:password, esAdmin=:esAdmin, ci=:ci, nombre=:nombre, apellidos=:apellidos
+                WHERE id_usuario=:id_usuario";
+        $this->db->prepare($query);
+        $this->db->bindParam(':usuario',$usuario->usuario);
+        $this->db->bindParam(':password',$usuario->password);
+        $this->db->bindParam(':esAdmin',$usuario->esAdmin);
+        $this->db->bindParam(':ci',$usuario->ci);
+        $this->db->bindParam(':nombre',$usuario->nombre);
+        $this->db->bindParam(':nombre',$usuario->apellido);
+        $this->db->bindParam(':id_usuario',$usuario->id_usuario);
+        return $this->db->execute();
+    }   
+    public function Eliminar($id)
     {
 
     }
