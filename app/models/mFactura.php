04e6476e72139bdab376b11eb6ab69d558c59751
Mauritius Gamarra <mauriciogamarravargas@gmail.com>
2019-03-18 09:26:46 -0400

    insertar factura

--- app/models/mFactura.php
+++ app/models/mFactura.php
@@ -13,6 +13,20 @@ class mFactura
                 cod_fiscal, tipo_sector, cod_autoverificador)
                 VALUES(:nit, :razon, :numero, :autorizacion, :fecha, :monto, :anulada, :sucursal, :modalidad, :tipo_emision, 
                 :cod_fiscal, :tipo_sector, :cod_autoverificador)";
-                
+                $this->db->prepare($query);
+                $this->db->bindParam(':nit',$factura->nit);
+                $this->db->bindParam(':razon',$factura->razon);
+                $this->db->bindParam(':numero',$factura->numero);
+                $this->db->bindParam(':autorizacion',$factura->autorizacion);
+                $this->db->bindParam(':fecha',$factura->fecha);
+                $this->db->bindParam(':monto',$factura->monto);
+                $this->db->bindParam(':anulada',$factura->anulada);
+                $this->db->bindParam(':sucursal',$factura->sucursal);
+                $this->db->bindParam(':modalidad',$factura->modalidad);
+                $this->db->bindParam(':tipo:emision',$factura->tipo_emision);
+                $this->db->bindParam(':cod_fiscal',$factura->cod_fiscal);
+                $this->db->bindParam(':tipo_sector',$factura->tipo_sector);
+                $this->db->bindParam(':cod_autoverificador',$factura->cod_autoverificador);
+                return $this->db->execute();
     }
 }
