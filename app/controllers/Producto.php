b331adb4216e07c2356c58564f272f9399f0fffd
Mauritius Gamarra <mauriciogamarravargas@gmail.com>
2019-03-21 17:03:26 -0400

    cambios controller productos mauricio

--- app/controllers/Producto.php
+++ app/controllers/Producto.php
@@ -6,23 +6,27 @@ class Producto extends Controller
         
         $productoP=['id_producto'=>1, 'descripcion'=>'Tinta negra epson 3xss', 'precio_unitario'=>'99.6', 'medida'=>'1L', 'categoria'=>'Chapi', 
                     'imagen'=>'', 'stock_minimo'=>'10', 'precio_compra'=>'50'];
-        var_dump($clienteP);
+        var_dump($ProductoP);
 
         $validador=new Validador();
-        $validador->Trim($clienteP);
-        $cliente=new Core\Cliente;
-        $cliente->id_cliente=1;
-        $cliente->nombre=$validador->Validar('nombre',['required','minlength,0','maxlength,25'],$clienteP);
-        $cliente->apellidos=$validador->Validar('apellidos',['required','minlength,0','maxlength,35'],$clienteP);
-        $cliente->razon=$validador->Validar('razon',['required','minlength,0','maxlenght,20'],$clienteP);
-        $cliente->nit=$validador->Validar('nit',['required','minlength,0','maxlenght,10'],$clienteP);
+        $validador->Trim($productoP);
+        $producto=new Core\Producto;
+        $producto->id_producto=1;
+        $producto->descripcion=$validador->Validar('descripcion',['required','minlength,0','maxlength,50'],$productoP);
+        $producto->precio_unitario=$validador->Validar('precio_unitario',['required'],$productoP);
+        $producto->medida=$validador->Validar('medida',['required','minlength,0','maxlenght,10'],$productoP);
+        $producto->categoria=$validador->Validar('categoria',['required'],$productoP);
+        $producto->imagen=$validador->Validar('imagen',['required'],$productoP);
+        $producto->stock_minimo=$validador->Validar('stock_minimo',['required','minlength,0','maxlenght,10'],$productoP);
+        $producto->precio_compra=$validador->Validar('precio_compra',['required'],$productoP);
 
-        var_dump($cliente);
+
+        var_dump($producto);
         var_dump($validador->error);
     }
     public function vRegistrar()
     {
-        $this->vista('Cliente/vRegistrar');
+        $this->vista('Producto/vRegistrar');
     }
 }
 
