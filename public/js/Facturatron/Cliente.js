296346f7d953141bd8ef38f8848c3092418257bc
Pablof3 <PabloHy2898@hotmail.com>
2019-03-24 19:18:26 -0400

    Crud Cliente

---
+++ public/js/Facturatron/Cliente.js
@@ -0,0 +1,22 @@
+var url = "http://"+window.location.host+"/Facturatron/"
+var form = document.getElementById("form_ClienteRegistro");
+form.addEventListener(
+    "submit",
+    function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        if(form.checkValidity() === true) {
+          $.ajax({
+              type: "POST",
+              url: url+'Cliente/Registrar',
+              data: $('#form_ClienteRegistro').serialize(),
+              dataType: "JSON",
+              success: function (response) {
+                console.log(response);
+              }
+          });  
+        }
+        form.classList.add("was-validated");
+    },
+    false
+);
