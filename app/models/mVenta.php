a6038646343cfecda20dfd3e977119d64a440adc
Mauritius Gamarra <mauriciogamarravargas@gmail.com>
2019-03-27 21:21:36 -0400

    modelo venta corregido insertar eliminar y editar

--- app/models/mVenta.php
+++ app/models/mVenta.php
@@ -31,54 +31,29 @@ class mVenta
         return $this->db->getRegistros();
     }
 
-
+    //Insertar
     public function Insertar($venta)
     {
-        try
-        {
-            $this->db->beginTransaction();
-            $nro_max = ObtenerNroMax();
-
+        $resp;
         $query="INSERT INTO Venta(nro, fecha, usuario, cliente, factura, total)
                 VALUES (:nro, :fecha, :usuario, :cliente, :factura, :total)";
         $this->db->prepare($query);
         $this->db->bindParam(':nro',$venta->nro);
         $this->db->bindParam(':fecha',$venta->fecha);
-        $this->db->bindParam(':usuario',$venta->esAdmin);
+        $this->db->bindParam(':usuario',$venta->esAdmin); //preguntar que chingados hace esto
         $this->db->bindParam(':cliente',$venta->cliente);
         $this->db->bindParam(':factura',$venta->factura);
         $this->db->bindParam(':total',$venta->total);
-        return $this->db->execute();
-        $id_venta = $this->db->lastInsertId();
-            
-        $query = "INSERT INTO VentaDetalle(venta, producto, cantidad, total) 
-                  VALUES(:venta, :producto, :cantidad, :total)";
-        $this->db->prepare($query);
-        foreach ($venta->venta_detalles as $key => $venta_detalle) {
-            $this->db->bindParam(":venta", $id_venta);
-            $this->db->bindParam(":producto", $venta_detalle->producto);
-            $this->db->bindParam(":cantidad", $venta_detalle->cantidad);
-            $this->db->bindParam(":subtotal", $venta_detalle->subtotal);				
-            $this->db->execute();
-        }   
-
-        $this->db->commit();
-        }
-        catch(Exception $ex)
-        {
-            $this->db->rollback();
-        }
-    }
-    public function ObtenerNroMax() 
-    {
-        $query = "SELECT MAX(nro) FROM Venta";
-        $this->db->prepare($query);
-        return $this->db->fetchColumn();
+        
+        $resp['status']=$this->db->execute();
+        $resp['error']=$this->db->error;
+        return $resp;
     }
 
-
+    //Actualizar
     public function Actualizar($venta)
     {
+        $resp;
         $query="UPDATE Venta
                 SET nro=:nro, fecha=:fecha, usuario=:usuario, cliente=:cliente, factura=:factura, total=:total
                 WHERE id_venta=:id_venta";
@@ -90,15 +65,22 @@ class mVenta
         $this->db->bindParam(':factura',$venta->factura);
         $this->db->bindParam(':total',$venta->total);
         $this->db->bindParam(':id_venta',$venta->id_venta);
-        return $this->db->execute();
-    }   
+        $resp['status']= $this->db->execute();
+        $resp['error']=$this->db->error;
+        return $resp;
+    }  
+    
+    //Eliminar
     public function Eliminar($id)
     {
+        $resp;
         $query="DELETE FROM Venta 
                 WHERE id_venta=:id_venta";
         $this->db->prepare($query);
         $this->db->bindParam(':id_venta', $id);
-        return $this->db->execute();
+        $resp['status']=$this->db->execute();
+        $resp['error']=$this->db->error;
+        return $resp;
     }
 }
 
