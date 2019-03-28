596c81d515a76786426cb667f43123af671abba6
Mauritius Gamarra <mauriciogamarravargas@gmail.com>
2019-03-27 21:04:13 -0400

    controller actualizar venta

--- app/controllers/Venta.php
+++ app/controllers/Venta.php
@@ -2,6 +2,7 @@
 class Venta extends Controller 
 {
 
+    //Registrar
     public function Registrar()
     {
         $resp['status']=true;
@@ -22,6 +23,35 @@ class Venta extends Controller
             $mVenta=new mVenta;
             $mresp=$mVenta->Insertar($venta);
             $resp['db']=Validador::ValidarDB($mresp);
+            $resp['status']=($resp['validate']['status'] && $resp['db']['status']);
+        }
+        echo json_encode($resp);
+    }
+
+
+    //Actualizar
+    public function Actualizar()
+    {
+        $resp['status']=true;
+        $validador=new Validador();
+        $validador->Trim($_POST['Venta']);
+
+        $venta=new Core\Venta;
+        $venta->id_venta=$validador->Validar('id_venta',['required','maxlength,11'],$_POST['Venta']);
+        $venta->nro=$validador->Validar('nro',['required','maxlength,11'],$_POST['Venta']);
+        $venta->fecha=$validador->Validar('fecha',['required'],$_POST['Venta']);
+        $venta->usuario=$validador->Validar('usuario',['required','minlength,0','maxlenght,11'],$_POST['Venta']);
+        $venta->cliente=$validador->Validar('cliente',['required','minlength,0','maxlenght,11'],$_POST['Venta']);
+        $venta->factura=$validador->Validar('factura',['required','minlength,0','maxlenght,11'],$_POST['Venta']);
+        $venta->total=$validador->Validar('total',['required','minlength,0','maxlenght,11'],$_POST['Venta']);
+
+        $resp['validate']=$validador->error;
+        $resp['status']=$resp['status']&&$resp['validate']['status'];
+        if ($validador->error['status']==true) 
+        {
+            $mVenta=new mVenta;
+            $mResp=$mVenta->Actualizar($venta);
+            $resp['db']=Validador::ValidarDB($mResp);
             $resp['status']=($resp['validate']['status'] && $resp['db']['status']);
         }
         echo json_encode($resp);
