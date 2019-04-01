a27236bc8d47520150f75ed8407525163853763c
Pablof3 <PabloHy2898@hotmail.com>
2019-04-01 09:34:36 -0400

    Sidebar Vista principal

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -53,12 +53,6 @@ $controlador=trim(explode('/',$_GET['url'])[0]);
                 </svg>
             </a>
 
-            <div class="search" data-search-path="Layouts.Search.html?q=">
-                <input placeholder="Search...">
-                <span class="search-icon">
-                    <i class="simple-icon-magnifier"></i>
-                </span>
-            </div>
         </div>
 
 
@@ -70,105 +64,6 @@ $controlador=trim(explode('/',$_GET['url'])[0]);
         <div class="navbar-right">
             <div class="header-icons d-inline-block align-middle">
                 
-                <div class="position-relative d-none d-sm-inline-block">
-                    <button class="header-icon btn btn-empty" type="button" id="iconMenuButton" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
-                        <i class="simple-icon-grid"></i>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-right mt-3  position-absolute" id="iconMenuDropdown">
-                        <a href="#" class="icon-menu-item">
-                            <i class="iconsmind-Equalizer d-block"></i>
-                            <span>Settings</span>
-                        </a>
-
-                        <a href="#" class="icon-menu-item">
-                            <i class="iconsmind-MaleFemale d-block"></i>
-                            <span>Users</span>
-                        </a>
-
-                        <a href="#" class="icon-menu-item">
-                            <i class="iconsmind-Puzzle d-block"></i>
-                            <span>Components</span>
-                        </a>
-
-                        <a href="#" class="icon-menu-item">
-                            <i class="iconsmind-Bar-Chart d-block"></i>
-                            <span>Profits</span>
-                        </a>
-
-                        <a href="#" class="icon-menu-item">
-                            <i class="iconsmind-File-Chart d-block"></i>
-                            <span>Surveys</span>
-                        </a>
-
-                        <a href="#" class="icon-menu-item">
-                            <i class="iconsmind-Suitcase d-block"></i>
-                            <span>Tasks</span>
-                        </a>
-
-                    </div>
-                </div>
-
-                <div class="position-relative d-inline-block">
-                    <button class="header-icon btn btn-empty" type="button" id="notificationButton" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
-                        <i class="simple-icon-bell"></i>
-                        <span class="count">3</span>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-right mt-3 scroll position-absolute" id="notificationDropdown">
-
-                        <div class="d-flex flex-row mb-3 pb-3 border-bottom">
-                            <a href="#">
-                                <img src="<?= RUTA_URL?>/img/profile-pic-l-2.jpg" alt="Notification Image" class="img-thumbnail list-thumbnail xsmall border-0 rounded-circle" />
-                            </a>
-                            <div class="pl-3 pr-2">
-                                <a href="#">
-                                    <p class="font-weight-medium mb-1">Joisse Kaycee just sent a new comment!</p>
-                                    <p class="text-muted mb-0 text-small">09.04.2018 - 12:45</p>
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="d-flex flex-row mb-3 pb-3 border-bottom">
-                            <a href="#">
-                                <img src="<?= RUTA_URL?>/img/notification-thumb.jpg" alt="Notification Image" class="img-thumbnail list-thumbnail xsmall border-0 rounded-circle" />
-                            </a>
-                            <div class="pl-3 pr-2">
-                                <a href="#">
-                                    <p class="font-weight-medium mb-1">1 item is out of stock!</p>
-                                    <p class="text-muted mb-0 text-small">09.04.2018 - 12:45</p>
-                                </a>
-                            </div>
-                        </div>
-
-
-                        <div class="d-flex flex-row mb-3 pb-3 border-bottom">
-                            <a href="#">
-                                <img src="<?= RUTA_URL?>/img/notification-thumb-2.jpg" alt="Notification Image" class="img-thumbnail list-thumbnail xsmall border-0 rounded-circle" />
-                            </a>
-                            <div class="pl-3 pr-2">
-                                <a href="#">
-                                    <p class="font-weight-medium mb-1">New order received! It is total $147,20.</p>
-                                    <p class="text-muted mb-0 text-small">09.04.2018 - 12:45</p>
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="d-flex flex-row mb-3 pb-3 ">
-                            <a href="#">
-                                <img src="<?= RUTA_URL?>/img/notification-thumb-3.jpg" alt="Notification Image" class="img-thumbnail list-thumbnail xsmall border-0 rounded-circle" />
-                            </a>
-                            <div class="pl-3 pr-2">
-                                <a href="#">
-                                    <p class="font-weight-medium mb-1">3 items just added to wish list by a user!</p>
-                                    <p class="text-muted mb-0 text-small">09.04.2018 - 12:45</p>
-                                </a>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
                 <button class="header-icon btn btn-empty d-none d-sm-inline-block" type="button" id="fullScreenButton">
                     <i class="simple-icon-size-fullscreen"></i>
                     <i class="simple-icon-size-actual"></i>
@@ -223,9 +118,14 @@ $controlador=trim(explode('/',$_GET['url'])[0]);
                             <i class="iconsmind-Administrator"></i> Usuarios
                         </a>
                     </li>
-                    <li class="active">
-                        <a href="<?=RUTA_URL?>/Usuario/vLista">
-                            <i class="iconsmind-Administrator"></i> Por El Error
+                    <li class="<?=($controlador=='Compra')?'active':''?>">
+                        <a href="<?=RUTA_URL?>/Compra/vLista">
+                            <i class=" iconsmind-Shopping-Cart"></i> Compra
+                        </a>
+                    </li>
+                    <li class="<?=($controlador=='Venta')?'active':''?>">
+                        <a href="<?=RUTA_URL?>/Venta/vLista">
+                            <i class=" iconsmind-Full-Cart"></i> Venta
                         </a>
                     </li>
                 </ul>
